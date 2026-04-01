e70dd0d197bdf455eaa915561d4ea988c36c3444
sengervinicius <senger.vinicius@gmail.com>
2026-04-01 20:54:30 +0100

    refactor: remove Chat from MobileMoreScreen nav

--- client/src/components/panels/MobileMoreScreen.jsx
+++ client/src/components/panels/MobileMoreScreen.jsx
@@ -88,11 +88,7 @@ const MobileMoreScreen = memo(({
           label="ETF Screener"
           onClick={() => onNavigate('etf')}
         />
-        <MenuItem
-          icon={<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" /></svg>}
-          label="Chat"
-          onClick={() => onNavigate('chat')}
-        />
+
       </div>
 
       {/* Settings & account */}
